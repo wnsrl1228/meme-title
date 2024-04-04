40fbde6ea52ac567bcbe39d1667d1f36ae5ed290
wnsrl <wnsrl981228@gmail.com>
2024-04-04 20:16:35 +0900

    feat : 페이징 관련 fetch join으로 수정

--- src/main/java/com/memetitle/comment/repository/CommentRepository.java
+++ src/main/java/com/memetitle/comment/repository/CommentRepository.java
@@ -3,13 +3,14 @@ package com.memetitle.comment.repository;
 import com.memetitle.comment.domain.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @EntityGraph(attributePaths = {"member"})
     Page<Comment> findByTitleId(Long titleId, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"member"})
     Page<Comment> findByMemberId(Long memberId, Pageable pageable);
 }
--- src/main/java/com/memetitle/meme/repository/TitleRepository.java
+++ src/main/java/com/memetitle/meme/repository/TitleRepository.java
@@ -3,12 +3,14 @@ package com.memetitle.meme.repository;
 import com.memetitle.meme.domain.Title;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TitleRepository extends JpaRepository<Title, Long> {
+
+    @EntityGraph(attributePaths = {"member"})
     Slice<Title> findByMemeId(Long memeId, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"member"})
     Slice<Title> findByMemberId(Long memberId, Pageable pageable);
 }
